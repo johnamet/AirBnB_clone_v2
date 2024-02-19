0ce1321ef3597013114f0a4aa16b2a9090f9fd51
Johnny <johnametepeagboku@live.com>
2024-02-19 20:42:55 +0000

    Update: Setup test database

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,14 +1,15 @@
---Create test database
+-- Create the database if it does not exist
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
---Create test user
+-- Create the user if it does not exist
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
---Grant privileges
-GRANT ALL PRIVILEGES  ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+-- Grant all privileges on the database hbnb_dev_db to the user hbnb_dev
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
---Grant select privilege on performance_schema
+-- Grant SELECT privilege on the performance_schema database to the user hbnb_dev
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 
---Flush everything to apply
+-- FLUSH privileges to apply changes
 FLUSH PRIVILEGES;
+
