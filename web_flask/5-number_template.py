817d59be6311cbc98b2ff6762cb2e8bb999fe75f
johnny <johnametepeagboku@gmail.com>
2024-03-22 07:40:27 +0000

    Task 5: Number Template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""
+Sample minimal flask app
+"""
+
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_route():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    return f'C {escape(text.replace("_", " "))}'
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def python_cool(text):
+    return f'Python {escape(text.replace("_", " "))}'
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/", strict_slashes=False)
+def default_python_cool():
+    default = "is cool"
+    return f"Python {default}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def is_number(n):
+    """
+    displays n if n is an integer
+
+    Returns:
+        str: if a n is a number
+    """
+
+    if type(n) == int:
+        return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def is_number_template(n):
+    """
+    display a html page
+
+    Returns:
+        str: a html page if n is int
+    """
+
+    if type(n) == int:
+        return f"<!DOCTYPE html> <HTML lang='en'>\
+                <HEAD><TITLE>HBNB</TITLE><BODY>\
+                <H1>Number: {n}</H1></BODY></HTML>"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
